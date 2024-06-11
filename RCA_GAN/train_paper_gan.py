9bf21c84ead6f239c22b01fcb7f617e8020bac1d
Bibal Sobeaux Pierre Gabriel <36329132+pierregab@users.noreply.github.com>
2024-06-11 13:04:59 +0200

    chore: Remove unused optimizer beta parameter in train_rca_gan function

--- RCA_GAN/train_paper_gan.py
+++ RCA_GAN/train_paper_gan.py
@@ -32,7 +32,7 @@ def main():
 
     # Train the model with the current hyperparameters
     train_rca_gan(
-        train_loader, val_loader, num_epochs=20, betas=(0.5, 0.999), init_type='xavier',
+        train_loader, val_loader, num_epochs=20, init_type='xavier',
     )
 
 if __name__ == '__main__':
