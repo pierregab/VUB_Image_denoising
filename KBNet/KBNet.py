57b7ed3aafab452101809204b269bd99cc5a15dc
Bibal Sobeaux Pierre Gabriel <36329132+pierregab@users.noreply.github.com>
2024-06-12 13:58:00 +0200

    implement KBNet model

--- KBNet/KBNet.py
+++ KBNet/KBNet.py
@@ -1,154 +1,234 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
+from torch.nn import init
 
-class KBAModule(nn.Module):
-    def __init__(self, in_channels, kernel_basis_count=32, kernel_size=3):
-        super(KBAModule, self).__init__()
-        self.kernel_basis_count = kernel_basis_count
-        self.kernel_size = kernel_size
+# LayerNorm2d and SimpleGate remain the same as in the provided reference code
+class LayerNormFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, weight, bias, eps):
+        ctx.eps = eps
+        N, C, H, W = x.size()
+        mu = x.mean(1, keepdim=True)
+        var = (x - mu).pow(2).mean(1, keepdim=True)
+        y = (x - mu) / (var + eps).sqrt()
+        weight, bias, y = weight.contiguous(), bias.contiguous(), y.contiguous()
+        ctx.save_for_backward(y, var, weight)
+        y = weight.view(1, C, 1, 1) * y + bias.view(1, C, 1, 1)
+        return y
 
-        # Learnable kernel bases
-        self.kernel_bases = nn.Parameter(torch.randn(kernel_basis_count, in_channels, kernel_size, kernel_size))
+    @staticmethod
+    def backward(ctx, grad_output):
+        eps = ctx.eps
+        N, C, H, W = grad_output.size()
+        y, var, weight = ctx.saved_tensors
+        g = grad_output * weight.view(1, C, 1, 1)
+        mean_g = g.mean(dim=1, keepdim=True)
+        mean_gy = (g * y).mean(dim=1, keepdim=True)
+        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
+        return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(dim=0), None
 
-        # Lightweight convolution branch to predict fusion coefficients
-        self.fusion_coeff_conv = nn.Sequential(
-            nn.Conv2d(in_channels, kernel_basis_count, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(kernel_basis_count, kernel_basis_count, kernel_size=3, padding=1)
-        )
-
-        # 1x1 convolution to transform the input feature map
-        self.feature_transform = nn.Conv2d(in_channels, in_channels, kernel_size=1)
+class LayerNorm2d(nn.Module):
+    def __init__(self, channels, eps=1e-6, requires_grad=True):
+        super(LayerNorm2d, self).__init__()
+        self.register_parameter('weight', nn.Parameter(torch.ones(channels), requires_grad=requires_grad))
+        self.register_parameter('bias', nn.Parameter(torch.zeros(channels), requires_grad=requires_grad))
+        self.eps = eps
 
     def forward(self, x):
-        batch_size, channels, height, width = x.size()
+        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
 
-        # Predict fusion coefficients
-        fusion_coeffs = self.fusion_coeff_conv(x)
+class SimpleGate(nn.Module):
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        return x1 * x2
 
-        # Transform input features
-        x_transformed = self.feature_transform(x)
+# KBAFunction and KBBlock_s as provided
+class KBAFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, att, selfk, selfg, selfb, selfw):
+        B, nset, H, W = att.shape
+        KK = selfk ** 2
+        selfc = x.shape[1]
+        att = att.reshape(B, nset, H * W).transpose(-2, -1)
+        ctx.selfk, ctx.selfg, ctx.selfc, ctx.KK, ctx.nset = selfk, selfg, selfc, KK, nset
+        ctx.x, ctx.att, ctx.selfb, ctx.selfw = x, att, selfb, selfw
+        bias = att @ selfb
+        attk = att @ selfw
+        uf = torch.nn.functional.unfold(x, kernel_size=selfk, padding=selfk // 2)
+        uf = uf.reshape(B, selfg, selfc // selfg * KK, H * W).permute(0, 3, 1, 2)
+        attk = attk.reshape(B, H * W, selfg, selfc // selfg, selfc // selfg * KK)
+        x = attk @ uf.unsqueeze(-1)
+        x = x.squeeze(-1).reshape(B, H * W, selfc) + bias
+        x = x.transpose(-1, -2).reshape(B, selfc, H, W)
+        return x
 
-        # Initialize the output feature map
-        output = torch.zeros_like(x_transformed)
+    @staticmethod
+    def backward(ctx, grad_output):
+        x, att, selfb, selfw = ctx.x, ctx.att, ctx.selfb, ctx.selfw
+        selfk, selfg, selfc, KK, nset = ctx.selfk, ctx.selfg, ctx.selfc, ctx.KK, ctx.nset
+        B, selfc, H, W = grad_output.size()
+        dbias = grad_output.reshape(B, selfc, H * W).transpose(-1, -2)
+        dselfb = att.transpose(-2, -1) @ dbias
+        datt = dbias @ selfb.transpose(-2, -1)
+        attk = att @ selfw
+        uf = F.unfold(x, kernel_size=selfk, padding=selfk // 2)
+        uf = uf.reshape(B, selfg, selfc // selfg * KK, H * W).permute(0, 3, 1, 2)
+        attk = attk.reshape(B, H * W, selfg, selfc // selfg, selfc // selfg * KK)
+        dx = dbias.view(B, H * W, selfg, selfc // selfg, 1)
+        dattk = dx @ uf.view(B, H * W, selfg, 1, selfc // selfg * KK)
+        duf = attk.transpose(-2, -1) @ dx
+        dattk = dattk.view(B, H * W, -1)
+        datt += dattk @ selfw.transpose(-2, -1)
+        dselfw = att.transpose(-2, -1) @ dattk
+        duf = duf.permute(0, 2, 3, 4, 1).view(B, -1, H * W)
+        dx = F.fold(duf, output_size=(H, W), kernel_size=selfk, padding=selfk // 2)
+        datt = datt.transpose(-1, -2).view(B, nset, H, W)
+        return dx, datt, None, None, dselfb, dselfw
 
-        # Fuse the kernel bases linearly to obtain adaptive and diverse aggregation weights
-        for i in range(self.kernel_basis_count):
-            basis = self.kernel_bases[i]
-            coeff = fusion_coeffs[:, i, :, :].unsqueeze(1)
-            output += F.conv2d(x_transformed, basis.unsqueeze(0), padding=self.kernel_size // 2, groups=1) * coeff
-
-        return output
-
-class MFFBlock(nn.Module):
-    def __init__(self, in_channels):
-        super(MFFBlock, self).__init__()
-
-        # Channel attention branch
-        self.channel_attention = nn.Sequential(
+class KBBlock_s(nn.Module):
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, nset=32, k=3, gc=4, lightweight=False):
+        super(KBBlock_s, self).__init__()
+        self.k, self.c = k, c
+        self.nset = nset
+        dw_ch = int(c * DW_Expand)
+        ffn_ch = int(FFN_Expand * c)
+        self.g = c // gc
+        self.w = nn.Parameter(torch.zeros(1, nset, c * c // self.g * self.k ** 2))
+        self.b = nn.Parameter(torch.zeros(1, nset, c))
+        self.init_p(self.w, self.b)
+        self.norm1 = LayerNorm2d(c)
+        self.norm2 = LayerNorm2d(c)
+        self.sca = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(in_channels, in_channels // 16, kernel_size=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels // 16, in_channels, kernel_size=1),
-            nn.Sigmoid()
+            nn.Conv2d(in_channels=c, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
         )
-
-        # Depthwise convolution branch
-        self.depthwise_conv = nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, groups=in_channels)
-
-        # KBA module branch
-        self.kba_module = KBAModule(in_channels)
-
-        # Layer normalization over channel dimension
-        self.layer_norm = nn.LayerNorm(in_channels)
-
-        # 1x1 convolution
-        self.pointwise_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
-
-    def forward(self, x):
-        # Layer normalization over channel dimension
-        x_norm = self.layer_norm(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
-
-        # Channel attention
-        ca = self.channel_attention(x_norm) * x_norm
-
-        # Depthwise convolution
-        dw = self.depthwise_conv(x_norm)
-
-        # KBA module
-        kba = self.kba_module(x_norm)
-
-        # Point-wise multiplication to fuse features
-        fused = ca * dw * kba
-
-        # 1x1 convolution to produce final output
-        output = self.pointwise_conv(fused)
-
-        return output
-
-class UNetKBNet(nn.Module):
-    def __init__(self, in_channels, out_channels, features=[64, 128, 256, 512]):
-        super(UNetKBNet, self).__init__()
-        self.encoder = nn.ModuleList()
-        self.decoder = nn.ModuleList()
-
-        # Encoder
-        for feature in features:
-            self.encoder.append(self._block(in_channels, feature))
-            in_channels = feature
-
-        # Bottleneck
-        self.bottleneck = self._block(features[-1], features[-1] * 2)
-
-        # Decoder
-        for feature in reversed(features):
-            self.decoder.append(
-                nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2)
+        if not lightweight:
+            self.conv11 = nn.Sequential(
+                nn.Conv2d(in_channels=c, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
+                nn.Conv2d(in_channels=c, out_channels=c, kernel_size=5, padding=2, stride=1, groups=c // 4, bias=True),
             )
-            self.decoder.append(self._block(feature * 2, feature))
-
-        # Final convolution
-        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
-
-    def forward(self, x):
-        skip_connections = []
-
-        # Encoder
-        for layer in self.encoder:
-            x = layer(x)
-            skip_connections.append(x)
-            x = F.max_pool2d(x, kernel_size=2, stride=2)
-
-        # Bottleneck
-        x = self.bottleneck(x)
-
-        # Decoder
-        skip_connections = skip_connections[::-1]
-        for idx in range(0, len(self.decoder), 2):
-            x = self.decoder[idx](x)
-            skip_connection = skip_connections[idx // 2]
-
-            if x.size() != skip_connection.size():
-                x = F.interpolate(x, size=skip_connection.size()[2:])
-
-            x = torch.cat((skip_connection, x), dim=1)
-            x = self.decoder[idx + 1](x)
-
-        return self.final_conv(x)
-
-    def _block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            MFFBlock(out_channels)
+        else:
+            self.conv11 = nn.Sequential(
+                nn.Conv2d(in_channels=c, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
+                nn.Conv2d(in_channels=c, out_channels=c, kernel_size=3, padding=1, stride=1, groups=c, bias=True),
+            )
+        self.conv1 = nn.Conv2d(in_channels=c, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv21 = nn.Conv2d(in_channels=c, out_channels=c, kernel_size=3, padding=1, stride=1, groups=c, bias=True)
+        interc = min(c, 32)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=c, out_channels=interc, kernel_size=3, padding=1, stride=1, groups=interc, bias=True),
+            SimpleGate(),
+            nn.Conv2d(interc // 2, self.nset, 1, padding=0, stride=1),
         )
+        self.conv211 = nn.Conv2d(in_channels=c, out_channels=self.nset, kernel_size=1)
+        self.conv3 = nn.Conv2d(in_channels=dw_ch // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_ch, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_ch // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.dropout1 = nn.Identity()
+        self.dropout2 = nn.Identity()
+        self.ga1 = nn.Parameter(torch.zeros((1, c, 1, 1)) + 1e-2, requires_grad=True)
+        self.attgamma = nn.Parameter(torch.zeros((1, self.nset, 1, 1)) + 1e-2, requires_grad=True)
+        self.sg = SimpleGate()
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)) + 1e-2, requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)) + 1e-2, requires_grad=True)
+
+    def init_p(self, weight, bias=None):
+        init.kaiming_uniform_(weight, a=math.sqrt(5))
+        if bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(weight)
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(bias, -bound, bound)
+
+    def KBA(self, x, att, selfk, selfg, selfb, selfw):
+        return KBAFunction.apply(x, att, selfk, selfg, selfb, selfw)
+
+    def forward(self, inp):
+        x = inp
+        x = self.norm1(x)
+        sca = self.sca(x)
+        x1 = self.conv11(x)
+        att = self.conv2(x) * self.attgamma + self.conv211(x)
+        uf = self.conv21(self.conv1(x))
+        x = self.KBA(uf, att, self.k, self.g, self.b, self.w) * self.ga1 + uf
+        x = x * x1 * sca
+        x = self.conv3(x)
+        x = self.dropout1(x)
+        y = inp + x * self.beta
+        x = self.norm2(y)
+        x = self.conv4(x)
+        x = self.sg(x)
+        x = self.conv5(x)
+        x = self.dropout2(x)
+        return y + x * self.gamma
+
+class KBNet(nn.Module):
+    def __init__(self, img_channel=1, width=64, middle_blk_num=12, enc_blk_nums=[2, 2, 4, 8], dec_blk_nums=[2, 2, 2, 2], basicblock='KBBlock_s', lightweight=False, ffn_scale=2):
+        super().__init__()
+        basicblock = eval(basicblock)
+        self.intro = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1, bias=True)
+        self.encoders = nn.ModuleList()
+        self.middle_blks = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+        self.ending = nn.Conv2d(in_channels=width, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1, bias=True)
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        chan = width
+        for num in enc_blk_nums:
+            self.encoders.append(
+                nn.Sequential(
+                    *[basicblock(chan, FFN_Expand=ffn_scale, lightweight=lightweight) for _ in range(num)]
+                )
+            )
+            self.downs.append(nn.Conv2d(chan, 2 * chan, 2, 2))
+            chan = chan * 2
+        self.middle_blks = nn.Sequential(
+            *[basicblock(chan, FFN_Expand=ffn_scale, lightweight=lightweight) for _ in range(middle_blk_num)]
+        )
+        for num in dec_blk_nums:
+            self.ups.append(
+                nn.Sequential(
+                    nn.Conv2d(chan, chan * 2, 1, bias=False),
+                    nn.PixelShuffle(2)
+                )
+            )
+            chan = chan // 2
+            self.decoders.append(
+                nn.Sequential(
+                    *[basicblock(chan, FFN_Expand=ffn_scale, lightweight=lightweight) for _ in range(num)]
+                )
+            )
+        self.padder_size = 2 ** len(self.encoders)
+
+    def forward(self, inp):
+        B, C, H, W = inp.shape
+        inp = self.check_image_size(inp)
+        x = self.intro(inp)
+        encs = []
+        for encoder, down in zip(self.encoders, self.downs):
+            x = encoder(x)
+            encs.append(x)
+            x = down(x)
+        x = self.middle_blks(x)
+        for decoder, up, enc_skip in zip(self.decoders, self.ups, encs[::-1]):
+            x = up(x)
+            x = x + enc_skip
+            x = decoder(x)
+        x = self.ending(x)
+        x = x + inp
+        return x[:, :, :H, :W]
+
+    def check_image_size(self, x):
+        _, _, h, w = x.size()
+        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
+        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
+        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
+        return x
 
 # Example usage
 if __name__ == "__main__":
-    model = UNetKBNet(in_channels=3, out_channels=1)
-    x = torch.randn(1, 3, 256, 256)  # Example input
+    model = KBNet(img_channel=1)
+    x = torch.randn(1, 1, 256, 256)  # Example input
     output = model(x)
     print(output.shape)
--- KBNet/train_KBNet.py
+++ KBNet/train_KBNet.py
@@ -4,8 +4,8 @@ import sys
 import os
 from torch import nn
 from torch.utils.tensorboard import SummaryWriter
-from KBNet import UNetKBNet
-import torchvision.transforms as transforms
+from torchvision import transforms
+from KBNet import KBNet  # Ensure this path is correct
 
 # Assuming your script is in RCA_GAN and the project root is one level up
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -18,15 +18,21 @@ gt_folder = 'DIV2K_train_HR.nosync/resized_ground_truth_images'
 degraded_folder = 'DIV2K_train_HR.nosync/degraded_images'
 
 def start_tensorboard(log_dir):
+    """Starts TensorBoard."""
     try:
         subprocess.Popen(['tensorboard', '--logdir', log_dir])
         print(f"TensorBoard started at http://localhost:6006")
     except Exception as e:
         print(f"Failed to start TensorBoard: {e}")
 
+def denormalize(tensor):
+    """Denormalizes a tensor from [-1, 1] to [0, 1]."""
+    return tensor * 0.5 + 0.5
+
 def train_unet_kbnet(train_loader, val_loader, writer, num_epochs=20, learning_rate=1e-3):
+    """Trains the UNet with KBNet module."""
     device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
-    model = UNetKBNet(in_channels=1, out_channels=1).to(device)
+    model = KBNet(img_channel=1).to(device)  # Correct the argument here
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -67,13 +73,13 @@ def train_unet_kbnet(train_loader, val_loader, writer, num_epochs=20, learning_r
             input_image, target_image = input_image.to(device), target_image.to(device)
             outputs = model(input_image)
 
-            img_grid_input = transforms.ToPILImage()(input_image[0].cpu())
-            img_grid_target = transforms.ToPILImage()(target_image[0].cpu())
-            img_grid_output = transforms.ToPILImage()(outputs[0].cpu())
+            example_degraded = denormalize(input_image[:4].cpu())
+            example_gt = denormalize(target_image[:4].cpu())
+            example_gen = denormalize(outputs[:4].cpu())
 
-            writer.add_image('Input Image', transforms.ToTensor()(img_grid_input), epoch)
-            writer.add_image('Ground Truth Image', transforms.ToTensor()(img_grid_target), epoch)
-            writer.add_image('Predicted Image', transforms.ToTensor()(img_grid_output), epoch)
+            writer.add_images('Degraded Images', example_degraded, epoch)
+            writer.add_images('Generated Images', example_gen, epoch)
+            writer.add_images('Ground Truth Images', example_gt, epoch)
 
 def main():
     log_dir = 'runs/unet_kbnet'
