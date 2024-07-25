4c6c6e2d558c3c3337fc8ca86c40f4821340a676
Bibal Sobeaux Pierre Gabriel <36329132+pierregab@users.noreply.github.com>
2024-07-25 11:43:51 +0200

    Refactor evaluate_model.py to consistently handle image shapes and convert RGB images to grayscale

--- evaluate_model.py
+++ evaluate_model.py
@@ -769,9 +769,9 @@ if __name__ == "__main__":
     train_noise_levels = [10, 20, 30, 40, 50, 60, 70, 80]
     val_noise_levels = [10, 20, 30, 40, 50, 60, 70, 80]
 
-    train_loader, val_loader = load_data(image_folder, batch_size=1, num_workers=8, validation_split=0.5, augment=False, dataset_percentage=0.001, only_validation=False, include_noise_level=True, train_noise_levels=train_noise_levels, val_noise_levels=val_noise_levels, use_rgb=True)
+    train_loader, val_loader = load_data(image_folder, batch_size=1, num_workers=8, validation_split=0.5, augment=False, dataset_percentage=0.01, only_validation=False, include_noise_level=True, train_noise_levels=train_noise_levels, val_noise_levels=val_noise_levels, use_rgb=True)
 
-    epochs_to_evaluate = [5]
+    epochs_to_evaluate = [50, 134]
     diffusion_model_paths = [f"checkpoints/diffusion_RDUNet_model_checkpointed_epoch_{epoch}.pth" for epoch in epochs_to_evaluate]
     unet_model_path = "checkpoints/rdunet_denoising.pth"
 
